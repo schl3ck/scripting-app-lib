4caa05cc94c12e5efa4579933db76125dd55c854
schl3ck <42554556+schl3ck@users.noreply.github.com>
2025-03-19 12:23:55 +0100

    complete MultiSelectionsPicker

--- MultiSelectionsPicker/multiSelectionsPicker.tsx
+++ MultiSelectionsPicker/multiSelectionsPicker.tsx
@@ -1,45 +1,80 @@
-import { HStack, Image, NavigationStack, Section, Spacer, Text, VirtualNode } from "scripting"
+import { Button, HStack, Image, List, NavigationStack, Section, Spacer, Text, VirtualNode, useState } from "scripting"
 
 export function MultiSelectionsPickerView({
   options,
   selections,
   onChanged,
+  selectAll,
+  selectNone,
 }: {
   options: string[] | VirtualNode[],
   selections: number[],
   onChanged: (selected: number[]) => void,
+  selectAll?: string,
+  selectNone?: string,
 }) {
-  const selected = new Set(selections)
+  if (!selectAll) selectAll = "Select all"
+  if (!selectNone) selectNone = "Select none"
+
+  const [selected, setSelected] = useState(() => new Set(selections))
 
   return <NavigationStack>
-    <Section>
-      {options.map((option, index) =>
-        <HStack
-          contentShape={"rect"}
-          onTapGesture={() => {
-            if (selected.has(index)) {
-              selected.delete(index)
-            } else {
-              selected.add(index)
-            }
-            onChanged(Array.from(selections))
-          }}
-        >
-          {
-            typeof option === "string"
-              ? <Text>{option}</Text>
-              : option
-          }
-          <Spacer />
-          {selected.has(index) ?
-            <Image
-              systemName={"checkmark"}
-              foregroundStyle={"accentColor"}
-            />
-            : null
-          }
+    <List
+      onDisappear={() => onChanged(Array.from(selected))}
+    >
+      <Section>
+        <HStack>
+          <Spacer minLength={0} />
+          <Button
+            title={selectAll}
+            action={() => {
+              setSelected(new Set(options.map((_, i) => i)))
+            }}
+            buttonStyle={"bordered"}
+            buttonBorderShape={"capsule"}
+          />
+          <Button
+            title={selectNone}
+            action={() => {
+              setSelected(new Set())
+            }}
+            buttonStyle={"bordered"}
+            buttonBorderShape={"capsule"}
+          />
+          <Spacer minLength={0} />
         </HStack>
-      )}
-    </Section>
+      </Section>
+      <Section>
+        {options.map((option, index) =>
+          <Button
+            foregroundStyle={"label"}
+            action={() => {
+              if (selected.has(index)) {
+                selected.delete(index)
+              } else {
+                selected.add(index)
+              }
+              setSelected(new Set(selected))
+            }}
+          >
+            <HStack>
+              {
+                typeof option === "string"
+                  ? <Text>{option}</Text>
+                  : option
+              }
+              <Spacer />
+              {selected.has(index) ?
+                <Image
+                  systemName={"checkmark"}
+                  foregroundStyle={"accentColor"}
+                />
+                : null
+              }
+            </HStack>
+          </Button>
+        )}
+      </Section>
+    </List>
   </NavigationStack>
 }
