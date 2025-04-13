00807dc69ae140bc267052ba5ffad1353705ead0
schl3ck <schleck2@gmx.at>
2025-04-13 18:56:22 +0200

    add some documentation to MultiSelectionsPicker

--- MultiSelectionsPicker/multiSelectionsPicker.tsx
+++ MultiSelectionsPicker/multiSelectionsPicker.tsx
@@ -1,5 +1,20 @@
-import { Button, HStack, Image, List, NavigationStack, Section, Spacer, Text, VirtualNode, useState } from "scripting"
+import {
+  Button,
+  HStack,
+  Image,
+  List,
+  NavigationStack,
+  Section,
+  Spacer,
+  Text,
+  VirtualNode,
+  useState,
+} from "scripting"
 
+/**
+ * A view to let the user select multiple options from a list. Use this as the destination of a
+ * `<NavigationLink>`.
+ */
 export function MultiSelectionsPickerView({
   options,
   selections,
@@ -7,74 +22,77 @@ export function MultiSelectionsPickerView({
   selectAll,
   selectNone,
 }: {
-  options: string[] | VirtualNode[],
-  selections: number[],
-  onChanged: (selected: number[]) => void,
-  selectAll?: string,
-  selectNone?: string,
+  /**
+   * The options from which the user can select. Can either be an array of
+   * strings or an array of custom JSX elements
+   */
+  options: string[] | VirtualNode[]
+  /** An array of the selected indices */
+  selections: number[]
+  /** Will be called with the new indices when the selection has changed */
+  onChanged: (selected: number[]) => void
+  /** The label of the button that selects all options when tapped */
+  selectAll?: string
+  /** The label of the button that selects no options when tapped */
+  selectNone?: string
 }) {
   if (!selectAll) selectAll = "Select all"
   if (!selectNone) selectNone = "Select none"
 
   const [selected, setSelected] = useState(() => new Set(selections))
 
-  return <NavigationStack>
-    <List
-      onDisappear={() => onChanged(Array.from(selected))}
-    >
-      <Section>
-        <HStack>
-          <Spacer minLength={0} />
-          <Button
-            title={selectAll}
-            action={() => {
-              setSelected(new Set(options.map((_, i) => i)))
-            }}
-            buttonStyle={"bordered"}
-            buttonBorderShape={"capsule"}
-          />
-          <Button
-            title={selectNone}
-            action={() => {
-              setSelected(new Set())
-            }}
-            buttonStyle={"bordered"}
-            buttonBorderShape={"capsule"}
-          />
-          <Spacer minLength={0} />
-        </HStack>
-      </Section>
-      <Section>
-        {options.map((option, index) =>
-          <Button
-            foregroundStyle={"label"}
-            action={() => {
-              if (selected.has(index)) {
-                selected.delete(index)
-              } else {
-                selected.add(index)
-              }
-              setSelected(new Set(selected))
-            }}
-          >
-            <HStack>
-              {
-                typeof option === "string"
-                  ? <Text>{option}</Text>
-                  : option
-              }
-              <Spacer />
-              {selected.has(index) ?
-                <Image
-                  systemName={"checkmark"}
-                  foregroundStyle={"accentColor"}
-                />
-                : null
-              }
-            </HStack>
-          </Button>
-        )}
-      </Section>
-    </List>
-  </NavigationStack>
+  return (
+    <NavigationStack>
+      <List onDisappear={() => onChanged(Array.from(selected))}>
+        <Section>
+          <HStack>
+            <Spacer minLength={0} />
+            <Button
+              title={selectAll}
+              action={() => {
+                setSelected(new Set(options.map((_, i) => i)))
+              }}
+              buttonStyle={"bordered"}
+              buttonBorderShape={"capsule"}
+            />
+            <Button
+              title={selectNone}
+              action={() => {
+                setSelected(new Set())
+              }}
+              buttonStyle={"bordered"}
+              buttonBorderShape={"capsule"}
+            />
+            <Spacer minLength={0} />
+          </HStack>
+        </Section>
+        <Section>
+          {options.map((option, index) => (
+            <Button
+              foregroundStyle={"label"}
+              action={() => {
+                if (selected.has(index)) {
+                  selected.delete(index)
+                } else {
+                  selected.add(index)
+                }
+                setSelected(new Set(selected))
+              }}
+            >
+              <HStack>
+                {typeof option === "string" ? <Text>{option}</Text> : option}
+                <Spacer />
+                {selected.has(index) ? (
+                  <Image
+                    systemName={"checkmark"}
+                    foregroundStyle={"accentColor"}
+                  />
+                ) : null}
+              </HStack>
+            </Button>
+          ))}
+        </Section>
+      </List>
+    </NavigationStack>
+  )
 }
--- MultiSelectionsPicker/testMultiSelectionsPicker.tsx
+++ MultiSelectionsPicker/testMultiSelectionsPicker.tsx
@@ -1,4 +1,4 @@
-import { Circle, Color, HStack, List, NavigationLink, NavigationStack, Text, useState } from "scripting"
+import { Circle, Color, HStack, List, NavigationLink, NavigationStack, Section, Text, useState } from "scripting"
 import { MultiSelectionsPickerView } from "./multiSelectionsPicker"
 
 const allItems: Color[] = [
@@ -24,38 +24,54 @@ export function TestMultiSelectionsPickerView() {
       navigationTitle="Multi Selections Picker"
       navigationBarTitleDisplayMode="inline"
     >
-      <NavigationLink
-        title="String options"
-        destination={
-          <MultiSelectionsPickerView
-            options={allItems}
-            selections={items}
-            onChanged={setItems}
-          />}
-      />
-      <NavigationLink
-        title="Custom options"
-        destination={
-          <MultiSelectionsPickerView
-            options={allItems.map(item =>
-              <HStack>
-                <Circle
-                  fill={item}
-                  frame={{
-                    width: 20,
-                    height: 20,
-                  }}
-                />
-                <Text>{item}</Text>
-              </HStack>
-            )}
-            selections={items}
-            onChanged={setItems}
-            selectAll="Select all"
-            selectNone="Select none"
-          />
+      <Section
+        footer={
+          <Text>
+            Multi select with simple string options
+          </Text>
         }
-      />
+      >
+        <NavigationLink
+          title="String options"
+          destination={
+            <MultiSelectionsPickerView
+              options={allItems}
+              selections={items}
+              onChanged={setItems}
+            />}
+        />
+      </Section>
+      <Section
+        footer={
+          <Text>
+            Multi select with custom elements for each option
+          </Text>
+        }
+      >
+        <NavigationLink
+          title="Custom options"
+          destination={
+            <MultiSelectionsPickerView
+              options={allItems.map(item =>
+                <HStack>
+                  <Circle
+                    fill={item}
+                    frame={{
+                      width: 20,
+                      height: 20,
+                    }}
+                  />
+                  <Text>{item}</Text>
+                </HStack>
+              )}
+              selections={items}
+              onChanged={setItems}
+              selectAll="Select all"
+              selectNone="Select none"
+            />
+          }
+        />
+      </Section>
     </List>
   </NavigationStack>
 }
